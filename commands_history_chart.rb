92b8f9aa06f9a9fcf608a7372749cd9be354d734
Rohit Patel <rohit.patel061@gmail.com>
2017-02-27 12:16:08 +0530

    [Fixes #1]Fix invalid utf-8 characters error

--- commands_history_chart.rb
+++ commands_history_chart.rb
@@ -1,13 +1,15 @@
 require 'json'
 shell_type = (`echo $SHELL`).split("/").last.chomp
 history_file = File.open(ENV['HOME'] + "/.#{shell_type}_history")
+history_file_contents = File.read(history_file).encode('UTF-8', :invalid=>:replace, :replace=>"?")
+history_file_contents.gsub!("\\", "")
 commands = []
 if shell_type.eql?("zsh")
-  File.read(history_file).split(";").each do |command|
+  history_file_contents.split(";").each do |command|
     commands << command.split(" ").first
   end
 elsif shell_type.eql?("bash")
-  File.read(history_file).split("\n").each do |command|
+  history_file_contents.split("\n").each do |command|
     commands << command.split(" ").first
   end
 else
@@ -27,7 +29,7 @@ File.open("commands_history.json", "w") do |f|
   f.write(commands_data.to_json)
 end
 data = File.read('commands_history.json')
-file_contents = File.read('commands_history_chart.js')
-file_contents.gsub!(/var commandsHistory;/, 'var commandsHistory = ' + data + ';')
-File.write('commands_history_chart.js', file_contents)
+lines = File.readlines('commands_history_chart.js')
+lines[1] = 'var commandsHistory = ' + data + ";\n"
+File.write('commands_history_chart.js', lines.join())
 system("/usr/bin/open -a '/Applications/Google Chrome.app' commands_history_chart.html")
